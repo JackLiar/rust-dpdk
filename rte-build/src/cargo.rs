94b00c6877d429ab8fe6855da76dfec136a7323e
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-01-11 11:16:58 +0800

    link dpdk libraries dynamically by default

--- rte-build/src/cargo.rs
+++ rte-build/src/cargo.rs
@@ -12,18 +12,18 @@ pub fn gen_cargo_config<S: AsRef<str>>(
     shared_libs: impl Iterator<Item = S>,
 ) {
     for lib in static_libs {
-        println!("cargo:rustc-link-lib=static={}", lib.as_ref());
+        println!("cargo:rustc-link-lib={}", lib.as_ref());
     }
 
     for lib in shared_libs {
         println!("cargo:rustc-link-lib={}", lib.as_ref());
     }
 
-    for dir in lib_dirs{
-        println!( "cargo:rustc-link-search=native={}", dir.as_ref());
+    for dir in lib_dirs {
+        println!("cargo:rustc-link-search=native={}", dir.as_ref());
     }
 
-    for dir in include_dirs{
+    for dir in include_dirs {
         println!("cargo:include={}", dir.as_ref());
     }
 }
